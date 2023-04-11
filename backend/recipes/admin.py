d1e501bb56805366620848d0f2236f761d845c60
Александр Майстришин <nuclear0077@gmail.com>
2023-04-11 20:10:40 +0300

    test

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -34,7 +34,7 @@ class RecipeAdmin(admin.ModelAdmin):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
-    #search_fields = ('name',)
+    # search_fields = ('name',)
     empty_value_display = '-пусто-'
 
 
